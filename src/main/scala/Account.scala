12cba5de9221735530e7bf9a15abb614ac84a0ae
Lau Llobet <lau.llobet@gmail.com>
2017-11-24 12:35:35 +0100

    versio mes funcional

--- src/main/scala/Account.scala
+++ src/main/scala/Account.scala
@@ -4,48 +4,36 @@ import java.util.concurrent.atomic.AtomicInteger
 object Account {
    val idCounter :AtomicInteger = new AtomicInteger(0)
   case class AccountSnapshot(date: LocalDate, amount: Double, balance: Double )
+  def apply():Account = Account(0,List.empty,idCounter.getAndIncrement())
 }
 
-class Account {
-
-  /**
-    * id should be unique for each new account. (Don't use RNG)
-    */
-  val id: Int = Account.idCounter.getAndIncrement()
-  var snapshots: List[Account.AccountSnapshot] = List.empty
+case class Account(balance: Double, snapshots: List[Account.AccountSnapshot], id: Int) {
 
   /**
     *
     * @return current balance of the account
     */
-  def balance: Double = math.floor(_balance*100.0)/100.0;
+  //def balance: Double = ;
 
-  def modifyBalance(amount: Double) = {
-    _balance += amount;
-    snapshots = Account.AccountSnapshot(LocalDate.now(),amount,balance) :: snapshots
+  def modifyBalance(amount: Double): Account = {
+    val newBalance = math.floor((balance + amount)*100.0)/100.0
+    Account(newBalance, Account.AccountSnapshot(LocalDate.now(),amount,balance) :: snapshots,id)
   }
 
-  private var _balance =  .0
-
-  def deposit(quantity: Double): Unit = {
-    if (quantity > 0) {
-      modifyBalance(quantity)
-    }
-  }
+  def deposit(quantity: Double): Account = if (quantity > 0) modifyBalance(quantity) else this
 
   /**
     *
     * @param quantity amount of money to withdrawal
     * @return money withdrawed. 0 if none
     */
-  def withdrawal(quantity: Double): Double = {
-    if (_balance < quantity || quantity <= 0)
-      0
-    else {
-      modifyBalance(-quantity)
-      quantity
-    }
-  }
+  def withdrawal(quantity: Double): (Account,Double) =  // falten els { per veure-ho clar de moment
+    if (balance < quantity || quantity <= 0)
+      (this, 0)
+    else
+      (modifyBalance(-quantity),quantity)
+
+
 
   /**
     * Prints all the history starting with the recent one.
--- src/test/scala/AccountSpec.scala
+++ src/test/scala/AccountSpec.scala
@@ -5,83 +5,84 @@ class AccountSpec extends WordSpec with Matchers {
 
   "Account" should {
     "not have the same if for 2 instances" in {
-      val c1 = new Account
-      val c2 = new Account
+      val c1 = Account()
+      val c2 = Account()
 
       c1.id shouldNot be { c2.id }
     }
 
     "have a balance 0 in the beginning" in {
-      val c = new Account
+      val c = Account()
 
       c.balance shouldBe 0
     }
 
     "have a balance that changes" in {
-      val c = new Account
+      val c = Account()
       val b1 = c.balance
-      c deposit 10
+      val c2 = c deposit 10
 
-      b1 + 10 shouldBe c.balance
+      b1 + 10 shouldBe c2.balance
     }
 
     "not allow balance to be changed directly" in {
-      "val a = new Account; a.balance = 10" shouldNot compile
+      "val a = Account(); a.balance = 10" shouldNot compile
     }
 
     "not allow a negative deposit" in {
-      val c = new Account
-      c deposit -10
+      val c = Account()
+      val c2 = c deposit -10
 
-      c.balance shouldBe 0
+      c2.balance shouldBe 0
     }
 
     "have a printStatements method" in {
-      val c = new Account
-      c.deposit(1000)
-      val amount = c.withdrawal(100)
-      c.printStatements()
+      val c = Account()
+      val c2 = c.deposit(1000)
+      val (c3,amount) = c2.withdrawal(100)
+      c3.printStatements()
       succeed
     }
 
     "remove the right ammount of money when withdrawal is called" in {
-      val c = new Account
-      c.deposit(1000)
-      val b1 = c.balance
-      c.withdrawal(100)
+      val c = Account()
+      val c2 = c.deposit(1000)
+      val b1 = c2.balance
+      val (c3,_) = c2.withdrawal(100)
 
-      c.balance + 100 shouldBe b1
+      c3.balance + 100 shouldBe b1
     }
 
     "return the amount of money removed when withdrawal" in {
-      val c = new Account
-      c.deposit(1000)
-      val amount = c.withdrawal(100)
+      val c = Account()
+      val c2 = c.deposit(1000)
+      val (_,amount) = c2.withdrawal(100)
 
       amount shouldBe 100
     }
 
     "return 0 and do nothing if there is not enough money for wihdrawal" in {
-      val c = new Account
-      c deposit 10
+      val c = Account()
+      val c2 = c deposit 10
 
-      c.withdrawal(1000) shouldBe 0
-      c.balance shouldBe 10
+      val (c3,withdrawal) = c2.withdrawal(1000)
+      withdrawal shouldBe 0
+      c3.balance shouldBe 10
     }
 
     "return 0 and do nothing if there is a negative withdrawal" in {
-      val c = new Account
-      val withdrawal = c withdrawal -10
+      val c = Account()
+      val (c2,withdrawal) = c withdrawal -10
 
       withdrawal shouldBe 0
-      c.balance shouldBe 0
+      c2.balance shouldBe 0
     }
 
     "limit precision to two decimals" in {
-      val c = new Account
-      c.deposit(10.1234)
+      val c = Account()
+      val c2 = c.deposit(10.1234)
 
-      c.balance shouldBe 10.12
+      c2.balance shouldBe 10.12
     }
   }
 }
